84b12add58061362f981e9f2c85010a9e94e4c7a
vibhajure <115398343+vibhajure@users.noreply.github.com>
2024-08-23 19:15:26 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "temp_vm1" {
+
+  ami = "ami-04cdc91e49cb06165"
+
+  instance_type = "t3.micro"
+  key_name = "vibhapardeep"
+ 
+
+  tags = {
+
+    Name = "Vibha6"
+
+
+  }
+
+}
---
+++ mybucket.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket  = "ryana-bucket3"
+  tags    = {
+	Name          = "MyS3Bucket"
+	Environment    = "vibha"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+     }
+     bucket_key_enabled = true
+       
+    
+  }
+}
